d4ec28097698017a1bd99e0021f49fd04dac856c
João Vidal <dev.joaovidal@gmail.com>
2022-07-02 15:15:23 -0300

    duracao challenge

---
+++ 05-estruturaCondicional/exercicios/duracao/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    
+
+
+   
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int initHour, finalHour;
+        
+        initHour = sc.nextInt();
+        finalHour = sc.nextInt();
+        
+        // ou 
+        // int initHour = sc.nextInt();
+        // int finalHour = sc.nextInt();
+
+        int duration;
+
+        if (initHour < finalHour) {
+            duration = finalHour - initHour;
+        }
+        else {
+            duration = 24 - initHour + finalHour;
+        }
+
+        System.out.println("O JOGO DUROU " + duration + " HORA(S)");
+
+    }
+}
