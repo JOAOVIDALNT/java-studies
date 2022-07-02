14ea7df851a38c20fe935adb9bc22683f2e641a2
João Vidal <dev.joaovidal@gmail.com>
2022-07-02 14:56:19 -0300

    multiplos challenge

---
+++ 05-estruturaCondicional/exercicios/multiplos/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A, B;
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        if (A % B == 0 || B % A == 0) {
+            System.out.println("SÃO MÚLTIPLOS");
+
+        }
+        else {
+            System.out.println("NÃO SÃO MÚLTIPLOS");
+        }
+
+
+    }
+    
+}
