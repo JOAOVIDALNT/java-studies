6d6c6c8eb81115b7c4aa99bfe2848ac4ec767029
João Vidal <dev.joaovidal@gmail.com>
2022-07-02 12:06:43 -0300

    salary challenge

---
+++ desafios/salario1/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int id;
+        int hours;
+        double pay;
+
+        id = sc.nextInt();
+        hours = sc.nextInt();
+        pay = sc.nextDouble();
+
+        double calc = hours * pay;
+
+        Locale.setDefault(Locale.US);
+
+        System.out.println("NUMBER = " + id);
+        System.out.printf("SALARY = R$%.2f%n", calc);
+
+
+    }
+
+}
