8365d8c01f1ac27654c955caaa18492ed10a2f2f
João Vidal <dev.joaovidal@gmail.com>
2022-07-02 11:30:05 -0300

    desafio1

---
+++ desafios/Soma1/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x;
+        int y;
+
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        System.out.println( "SOMA");
+        System.out.println(x + y);
+
+        sc.close();
+
+
+        
+    }
+}
