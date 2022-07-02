3496146762f3605b3a3558e91fb7214eb354c636
João Vidal <dev.joaovidal@gmail.com>
2022-07-02 14:42:07 -0300

    if-else first challenge

---
+++ 05-estruturaCondicional/exercicios/negativo/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    
+
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        num = sc.nextInt();
+
+        if (num < 0) {
+            System.out.println("NEGATIVO");
+        }
+        else {
+            System.out.println("POSITIVO");
+        }
+
+    }
+
+}
