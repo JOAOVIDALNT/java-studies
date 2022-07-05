6d3ce8b18db9159a553d4c65a8b7d72d6f6eda85
João Vidal <dev.joaovidal@gmail.com>
2022-07-05 14:10:26 -0300

    update

---
+++ 06-estruturasRepetitivas/while/testeDeMesa/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+        public static void main(String[] args) {
+            
+            int x = 0;
+            int y = 4;
+
+            while (x < 3) {
+                y += 2;
+                x += 1;
+
+                System.out.println(x + "-" + y);
+            }
+
+            System.out.println("programa terminado");
+
+
+
+        }
+
+
+
+}
