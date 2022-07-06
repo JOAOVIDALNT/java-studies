c760247b14d3ca9012f8148871bffc7ba1e3a763
João Vidal <dev.joaovidal@gmail.com>
2022-07-06 20:05:21 -0300

    add example fo

---
+++ 06-estruturasRepetitivas/for/exemplo/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    
+
+    public static void main(String[] args) {
+
+        // define a variavel no inicio da aplicação
+        
+        // incrementa 
+        // checa se a condição é verdadeira
+        
+        for (int i=0; i<101 ; i++) {
+            System.out.println("Valor de i: " + i);
+        }
+    }
+}
