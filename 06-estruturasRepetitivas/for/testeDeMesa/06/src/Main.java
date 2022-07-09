e137f487a5c0730fe4f8c66fa026fa4f20e2258e
João Vidal <dev.joaovidal@gmail.com>
2022-07-09 15:26:31 -0300

    add 06 table test

---
+++ 06-estruturasRepetitivas/for/testeDeMesa/06/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    
+
+
+    public static void main(String[] args) {
+        
+        int x = 4;
+        int y = 0;
+
+        for (int i = 0; i < x; i++) {
+            y += 1;
+        }
+
+        System.out.println(y);
+
+        // 4
+        
+            
+        
+
+    }
+}
