f1cdc2c7b6a9eb22d5c48465999b947c46153dc6
João Vidal <dev.joaovidal@gmail.com>
2022-07-09 15:22:51 -0300

    add 05

---
+++ 06-estruturasRepetitivas/for/testeDeMesa/05/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 4;
+        int y = 0;
+         
+        for ( int i = 0; i < x; i++) {
+            System.out.print(i);
+            System.out.println(x);
+            y += 10;
+            
+        }
+
+        // 04 y10
+        // 14 y20
+        // 24 y30
+        // 34 y40
+
+
+
+    }
+    
+}
