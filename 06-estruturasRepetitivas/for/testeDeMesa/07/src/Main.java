a0a5007900ada0e76d17e33f25411325eb890c5c
João Vidal <dev.joaovidal@gmail.com>
2022-07-09 15:31:35 -0300

    finishing fog  for table tests

---
+++ 06-estruturasRepetitivas/for/testeDeMesa/07/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int x = 8;
+        int y = 3;
+
+        for (int i = 0; y < x; i++ ) {
+            x -= 2;
+            y += 1;
+            System.out.println(i);
+        }
+    }
+
+    // 0 | x = 6 | y = 4
+    // 1 | x = 4 | y = 5
+    
+}
