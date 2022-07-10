a129c543873dd8efa74c51667246157e165d1e79
João Vidal <dev.joaovidal@gmail.com>
2022-07-09 21:01:58 -0300

    adding string functions

---
+++ 07-outrosTopicos/funcoesParaString.md
@@ -0,0 +1,17 @@
+#### formatar
+- toLowerCase() -> minusculo
+- toUpperCase() -> maiusculo
+- trim() -> remove espaços
+
+#### recortar
+- substring(inicio)
+- substring(inicio, fim)
+- str.Split("")
+
+#### substituir
+- Replace(char, char)
+- Replace(string, string)
+
+#### buscar
+- indexOf
+- LastIndexOf
---
+++ 07-outrosTopicos/split/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    
+
+    public static void main(String[] args) {
+        
+        String s = "potato apple lemon";
+
+        String[] vect = s.split(" "); // separador: " " -> espaço
+
+        System.out.println(vect[0]);
+        System.out.println(vect[1]);
+        System.out.println(vect[2]);
+        
+
+
+
+    }
+
+
+}
