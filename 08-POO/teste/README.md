23a1b957d983b950b81d60a3b355878b98af8ad5
João Vidal <dev.joaovidal@gmail.com>
2022-07-13 18:46:14 -0300

    exercicio sem OO

---
+++ 08-POO/teste/README.md
@@ -0,0 +1,1 @@
+### Exercicio sem Orientação a objetos
--- 08-POO/teste1/src/com/Package/Main.java
+++
@@ -1,7 +0,0 @@
-package com.Package;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("RODANDO");
-    }
-}
