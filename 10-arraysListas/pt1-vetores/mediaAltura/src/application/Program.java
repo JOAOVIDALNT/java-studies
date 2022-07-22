955f071d1eb1f253bce871826a5105b324421734
João Vidal <dev.joaovidal@gmail.com>
2022-07-22 14:02:59 -0300

    adding text to height avarage

--- 10-arraysListas/pt1-vetores/mediaAltura/src/application/Program.java
+++ 10-arraysListas/pt1-vetores/mediaAltura/src/application/Program.java
@@ -8,11 +8,13 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("How many properties you want to get avarage of?: ");
         int n = sc.nextInt();
         double[] vect = new double[n]; /* Cria um vetor, o 'n' define a
         quantidade de elementos no vetor e double define o tipo. */
 
         for (int  i = 0; i < n; i++) {
+            System.out.print("Enter a height: ");
             vect[i] = sc.nextDouble(); /* Váriavel i começa na posição zero,
             logo a posição no vect vai corresponder. */
         }
