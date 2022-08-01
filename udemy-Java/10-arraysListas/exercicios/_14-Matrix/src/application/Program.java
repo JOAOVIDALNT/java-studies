5fa6e0ca4cea2df3f8ac451af1b9917984ee5a68
João Vidal <dev.joaovidal@gmail.com>
2022-08-01 19:46:13 -0300

    updating matrix exercise

--- udemy-Java/10-arraysListas/exercicios/_14-Matrix/src/application/Program.java
+++ udemy-Java/10-arraysListas/exercicios/_14-Matrix/src/application/Program.java
@@ -11,20 +11,20 @@ public class Program {
 
         int[][] matrix = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
 
         System.out.print("Main Diagonal: ");
-        for (int i = 0; i<n; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             System.out.printf("%d ", matrix[i][i]);
         }
 
         int count = 0;
-        for (int i = 0; i<n; i++) {
-            for (int j = 0; j<n; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if(matrix[i][j] < 0) {
                     count++;
                 }
