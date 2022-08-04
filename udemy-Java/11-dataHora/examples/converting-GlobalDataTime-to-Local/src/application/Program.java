e4741f9173d7632215078ea760e410327b8fcfcf
João Vidal <dev.joaovidal@gmail.com>
2022-08-03 21:17:06 -0300

    folder managment

---
+++ udemy-Java/11-dataHora/examples/converting-GlobalDataTime-to-Local/src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class Program {
+    public static void main(String[] args) {
+
+        LocalDate d01 = LocalDate.parse("2022-08-03");
+        LocalDateTime dt01 = LocalDateTime.parse("2022-08-03T20:36");
+        Instant i01 = Instant.parse("2022-08-03T20:36:20Z");
+
+        LocalDate.of
+
+//            for (String x : ZoneId.getAvailableZoneIds()) {
+//            System.out.println(x); // imprime todos os zoneIds disponíveis ex:"America/Sao_Paulo" 600+ ids
+//        }
+
+
+    }
+}
