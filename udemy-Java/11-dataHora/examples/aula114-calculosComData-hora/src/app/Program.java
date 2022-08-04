71994e51a1fb0afb0f3c962a5876e692900aa7f7
João Vidal <dev.joaovidal@gmail.com>
2022-08-04 14:09:12 -0300

    finishing data-time by calculating

--- udemy-Java/11-dataHora/examples/aula114-calculosComData-hora/src/app/Program.java
+++ udemy-Java/11-dataHora/examples/aula114-calculosComData-hora/src/app/Program.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -18,7 +19,7 @@ public class Program {
 //        System.out.println(pastWeekLocalDate);
 //        System.out.println(nextWeekLocalDate);
 
-
+        LocalDateTime pastMonthLocalDateTime = dt01.minusMonths(1);
         LocalDateTime nextHourLocalDateTime = dt01.plusHours(1);
         // we also have plusMinutes, plusSeconds, plusNanos (for nano seconds)
         // as well we have day, weeks, month, years...
@@ -29,6 +30,21 @@ public class Program {
 
         System.out.println(pastMinutesInstant);
 
+        Duration t1 = Duration.between(nextHourLocalDateTime, dt01);
+        Duration t2 = Duration.between(pastMonthLocalDateTime, dt01);
+
+        Duration t3 = Duration.between(pastWeekLocalDate.atTime(0, 0), d01.atTime(0,0));
+        Duration t4 = Duration.between(pastWeekLocalDate.atStartOfDay(), d01.atStartOfDay()); // same thing
+        // use .atTime or another at function to set LocalDate variables since they're not accepted w/ time
+
+        Duration t5 = Duration.between(pastMinutesInstant, i01);
+
+        System.out.println(t1);
+        System.out.println(t2);
+
+        System.out.println(t3);
+        System.out.println(t4);
+        System.out.println(t5);
 
 
 
