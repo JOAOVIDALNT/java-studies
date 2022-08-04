06af5221abde08b838f4813d11aa5628eba30b82
João Vidal <dev.joaovidal@gmail.com>
2022-08-04 13:48:25 -0300

    unity of measures to set data-time

--- udemy-Java/11-dataHora/examples/aula114-calculosComData-hora/src/app/Program.java
+++ udemy-Java/11-dataHora/examples/aula114-calculosComData-hora/src/app/Program.java
@@ -3,6 +3,7 @@ package app;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Program {
     public static void main(String[] args) {
@@ -12,8 +13,24 @@ public class Program {
         Instant i01 = Instant.parse("2022-08-03T01:36:20Z");
 
         LocalDate pastWeekLocalDate = d01.minusWeeks(1);
+        LocalDate nextWeekLocalDate = d01.plusWeeks(1);
+        //                               .plusDays / .minusDays / Months, Years...
+//        System.out.println(pastWeekLocalDate);
+//        System.out.println(nextWeekLocalDate);
 
-        System.out.println(pastWeekLocalDate);
+
+        LocalDateTime nextHourLocalDateTime = dt01.plusHours(1);
+        // we also have plusMinutes, plusSeconds, plusNanos (for nano seconds)
+        // as well we have day, weeks, month, years...
+
+        Instant pastMinutesInstant = i01.minus(7, ChronoUnit.MINUTES);
+        // ChronoUnit also have measures like decades, eras, centuries, miles, days, halfDays etc..
+
+
+        System.out.println(pastMinutesInstant);
+
+
+
 
 
 
