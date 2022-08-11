d322c72b063aa6ce11fca88d1cba6cf35dd0a64e
João Vidal <dev.joaovidal@gmail.com>
2022-08-10 21:01:29 -0300

    Enum exercise 2 with StringBuilder

---
+++ udemy-Java/13-enum/exercises/PostComment/src/Entities/Comment.java
@@ -0,0 +1,24 @@
+package Entities;
+
+import java.text.SimpleDateFormat;
+
+public class Comment {
+
+    private String text;
+
+    public Comment() {
+
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
