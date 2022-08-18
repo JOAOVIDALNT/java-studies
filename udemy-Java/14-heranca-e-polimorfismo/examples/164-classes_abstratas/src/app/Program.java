69f01d3b909216b3cfeb77d1834ba84563c3d601
João Vidal <dev.joaovidal@gmail.com>
2022-08-18 10:47:59 -0300

    abstract classes

---
+++ udemy-Java/14-heranca-e-polimorfismo/examples/164-classes_abstratas/src/app/Program.java
@@ -0,0 +1,43 @@
+package app;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+//        Account acc1 = new Account(101, "Pedro", 200.00); -> não instância pois a classe Account agora é abstrata
+//        Account acc2 = new SavingsAccount(102, "Flavia", 200.00, 0.01);
+//        Account acc3 = new BusinessAccount(103, "Mario", 200.00, 500.00);
+
+        List<Account> list = new ArrayList<>();
+
+        list.add(new SavingsAccount(001, "João", 1000.00, 0.01));
+        list.add(new BusinessAccount(002, "Giulia", 3000.00, 1000.00));
+        list.add(new BusinessAccount(003, "João", 2000.00, 2000.00));
+        list.add(new SavingsAccount(004, "Giulia", 800.00, 0.01));
+
+        double sum = 0.0;
+
+        for (Account acc : list) {
+            sum += acc.getBalance();
+        }
+
+        System.out.printf("Total balance: %.2f%n", sum);
+
+        for (Account acc : list) {
+            acc.deposit(20.0);
+        }
+
+        for (Account acc : list) {
+            System.out.printf("Account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+        }
+
+
+    }
+}
