1ce17a6904a6b85748daa61dfffd27ab12ebc39f
João Vidal <dev.joaovidal@gmail.com>
2022-08-18 11:06:42 -0300

    update abstract classes

--- udemy-Java/14-heranca-e-polimorfismo/examples/164-classes_abstratas/src/app/Program.java
+++ udemy-Java/14-heranca-e-polimorfismo/examples/164-classes_abstratas/src/app/Program.java
@@ -15,11 +15,12 @@ public class Program {
 //        Account acc2 = new SavingsAccount(102, "Flavia", 200.00, 0.01);
 //        Account acc3 = new BusinessAccount(103, "Mario", 200.00, 500.00);
 
+        Account account = new SavingsAccount();
         List<Account> list = new ArrayList<>();
 
         list.add(new SavingsAccount(001, "João", 1000.00, 0.01));
-        list.add(new BusinessAccount(002, "Giulia", 3000.00, 1000.00));
-        list.add(new BusinessAccount(003, "João", 2000.00, 2000.00));
+        list.add(new BusinessAccount(002, "Giulia", 4000.00, 2000.00));
+        list.add(new BusinessAccount(003, "João", 3000.00, 2000.00));
         list.add(new SavingsAccount(004, "Giulia", 800.00, 0.01));
 
         double sum = 0.0;
@@ -31,9 +32,10 @@ public class Program {
         System.out.printf("Total balance: %.2f%n", sum);
 
         for (Account acc : list) {
-            acc.deposit(20.0);
+            acc.deposit(1000.0);
         }
 
+        System.out.println("DEPOSIT");
         for (Account acc : list) {
             System.out.printf("Account %d: %.2f%n", acc.getNumber(), acc.getBalance());
         }
