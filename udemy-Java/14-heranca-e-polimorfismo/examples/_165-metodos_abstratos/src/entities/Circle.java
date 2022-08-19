9656d24c81fb3074d2d59f168777907458cea44c
João Vidal <dev.joaovidal@gmail.com>
2022-08-19 20:48:21 -0300

    Métodos abstratos example

---
+++ udemy-Java/14-heranca-e-polimorfismo/examples/_165-metodos_abstratos/src/entities/Circle.java
@@ -0,0 +1,22 @@
+package entities;
+
+import entities.enums.Color;
+
+public class Circle extends Shape {
+
+    private Double radius;
+
+    public Circle() { super(); }
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return  Math.PI * radius * radius;
+    }
+
+
+}
