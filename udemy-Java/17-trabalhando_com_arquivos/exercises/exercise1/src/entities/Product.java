c0e9e67d476ae1481da6f30c1a812151d0adfaf5
João Vidal <dev.joaovidal@gmail.com>
2022-09-16 22:20:41 -0300

    file managment exercise

---
+++ udemy-Java/17-trabalhando_com_arquivos/exercises/exercise1/src/entities/Product.java
@@ -0,0 +1,42 @@
+package entities;
+
+public class Product {
+    private String name;
+    private Integer quantity;
+    private Double price;
+
+
+    public Product(String name, Integer quantity, Double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public double total() {
+        return quantity * price;
+    }
+}
