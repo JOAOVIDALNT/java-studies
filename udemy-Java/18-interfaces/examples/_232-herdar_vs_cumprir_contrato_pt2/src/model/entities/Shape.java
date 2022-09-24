283975e9a4b2addd938589ca9f18b94a42d74e7c
João Vidal <dev.joaovidal@gmail.com>
2022-09-23 22:35:18 -0300

    implementing with interface and abstract class

---
+++ udemy-Java/18-interfaces/examples/_232-herdar_vs_cumprir_contrato_pt2/src/model/entities/Shape.java
@@ -0,0 +1,9 @@
+package model.entities;
+
+
+import model.enums.Color;
+
+public interface Shape {
+
+    double area(); // todo objeto que for uma figura (shape) terá que implementar a operação area()
+}
