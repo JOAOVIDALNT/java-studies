b6d687ef48bcfd9bff53930cc631ed8603362d24
João Vidal <dev.joaovidal@gmail.com>
2022-11-05 15:56:17 -0300

    map exercise

--- udemy-Java/19-generics_set_map/exercises/_249-Exercicio_fixacao_map/src/app/Program.java
+++ udemy-Java/19-generics_set_map/exercises/_249-Exercicio_fixacao_map/src/app/Program.java
@@ -1,13 +1,45 @@
 package app;
 
-import entities.Candidate;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Program {
 
-    Map<Candidate, Integer> votes = new TreeMap<>();
+    public static void main(String[] args) {
 
-    
+        Map<String, Integer> votes = new LinkedHashMap<>();
+
+        String path = "C:\\projects\\java\\java-studies\\udemy-Java\\19-generics_set_map\\exercises\\_249-Exercicio_fixacao_map\\votes.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+
+            while(line != null) {
+                String[] fields = line.split(",");
+                String name = fields[0];
+                Integer voteCount = Integer.parseInt(fields[1]);
+
+                if (votes.containsKey(name)) {
+                    int votesSoFar = votes.get(name);
+                    votes.put(name, voteCount + votesSoFar);
+                } else {
+                    votes.put(name, voteCount);
+                }
+
+                line = br.readLine();
+            }
+            for (String key : votes.keySet()) {
+                System.out.println(key + ": " + votes.get(key));
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
 }
