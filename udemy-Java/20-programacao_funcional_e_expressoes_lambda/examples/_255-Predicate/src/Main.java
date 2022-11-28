a16f4822ff8de257c8dfc20274f466f2da9b5e31
João Vidal <dev.joaovidal@gmail.com>
2022-11-27 21:05:07 -0300

    predicate comments refactor

--- udemy-Java/20-programacao_funcional_e_expressoes_lambda/examples/_255-Predicate/src/Main.java
+++ udemy-Java/20-programacao_funcional_e_expressoes_lambda/examples/_255-Predicate/src/Main.java
@@ -15,15 +15,15 @@ public class Main {
         list.add(new Product("Headset", 90.00));
         list.add(new Product("SSD", 130.00));
 
-        list.removeIf(p -> p.getPrice() >= 100.00);
+        list.removeIf(p -> p.getPrice() >= 100.00); // Expressão lambda inline
 
 
-//        list.remove(new ProductPredicate()); -> aplicando através da interface funcional
-//        list.removeIf(Product::staticProductPredicate); -> aplicando através de um método estático dentro da classe product
-//        list.removeIf(Product::nonStaticProductPredicate); -> através de um método não estático
+//        list.remove(new ProductPredicate()); // Implementação da interface
+//        list.removeIf(Product::staticProductPredicate); // Reference method com método estático
+//        list.removeIf(Product::nonStaticProductPredicate); // Reference method com método não estático
 
-//        Predicate<Product> pred = p -> p.getPrice() >= 100.0;
-//        list.removeIf(pred);
+//        Predicate<Product> pred = p -> p.getPrice() >= 100.0; // Expressão lambda declarada
+//        list.removeIf(pred);                                 // Expressão lambda declarada
 
 
         for (Product p : list) {
