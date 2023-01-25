2000b8a7b9c6045f8f5751ec3bdb9786ebc4b92e
João Vidal <dev.joaovidal@gmail.com>
2023-01-25 13:48:12 -0300

    starting spring-studies

---
+++ spring-studies/livro-spring_boot/first_spring_project/src/main/java/com/example/first_spring_project/FirstSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.first_spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringProjectApplication.class, args);
+    }
+
+}
