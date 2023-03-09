72254cb3314485714a00a7760d0cd40fd2ccf692
João Vidal <dev.joaovidal@gmail.com>
2023-03-09 14:07:58 -0300

    implementing findAll with mock

--- courses/udemy/spring-java-docker-aws/07-como_usar_os_verbos_get_post_put_delete/verbos_http/src/main/java/com/example/verbos_http/controllers/PersonController.java
+++ courses/udemy/spring-java-docker-aws/07-como_usar_os_verbos_get_post_put_delete/verbos_http/src/main/java/com/example/verbos_http/controllers/PersonController.java
@@ -6,17 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
     @Autowired
     private PersonServices service;
+    // ao invés de private PersonServices service = new Service(); também vale pra injeção em construtor
 
-    // Utiliza-se métodos mais antigos pora hora para ficar a par caso seja necessário manutenção de legado.
+    // Utiliza-se métodos mais antigos por hora para ficar a par caso seja necessário manutenção de legado.
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable(value = "id") String id) throws Exception {
+    public Person findById(@PathVariable(value = "id") String id) {
         return service.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
     }
 
 
--- courses/udemy/spring-java-docker-aws/07-como_usar_os_verbos_get_post_put_delete/verbos_http/src/main/java/com/example/verbos_http/services/PersonServices.java
+++ courses/udemy/spring-java-docker-aws/07-como_usar_os_verbos_get_post_put_delete/verbos_http/src/main/java/com/example/verbos_http/services/PersonServices.java
@@ -3,6 +3,8 @@ package com.example.verbos_http.services;
 import com.example.verbos_http.models.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 @Service // o spring boot encara a classe como um objeto que vai ser injetado no runtime em outras classes da aplicação, possibilita usar o @Autowired e não precisa instanciar
@@ -11,10 +13,24 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+
+    public List<Person> findAll() {
+        logger.info("Finding all");
+        // seria aqui onde acessariamos a base de dados pra recuperar a listagem
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+
     public Person findById(String id) {
         logger.info("Finding one person!");
+        // seria aqui onde acessariamos a base de dados pra recuperar a listagem
 
-
+        // MOCK
         Person person  = new Person();
         person.setId(counter.incrementAndGet());
         person.setFirstName("João");
@@ -22,9 +38,19 @@ public class PersonServices {
         person.setAddress("São Paulo - SP, Brazil");
         person.setGender("Male");
         return person;
+        //
+        // mock é uma estrutura de código temporária que sustenta o desenvolvimento do código até que os outros componentes estejam prontos
     }
 
-    // mock é uma estrutura de código temporária que sustenta o desenvolvimento do código até que os outros componentes estejam prontos
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Somewhere in Brazil");
+        person.setGender("Male");
+        return person;
+    }
 
 
 
