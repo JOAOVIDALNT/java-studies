5ea7006396fedf8afa294382e04fc6d9e822ad2c
João Vidal <dev.joaovidal@gmail.com>
2023-03-14 22:09:26 -0300

    spring-java-docker-aws: implementing content negotiation through header params

--- courses/udemy/spring-java-docker-aws/13-content_negotioation/content_negotiation/src/main/java/com/example/integrando_database/config/WebConfig.java
+++ courses/udemy/spring-java-docker-aws/13-content_negotioation/content_negotiation/src/main/java/com/example/integrando_database/config/WebConfig.java
@@ -8,11 +8,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorParameter(true)
-                .parameterName("mediaType").ignoreAcceptHeader(true)
+
+        /*
+        * CONFIG PARA OBTER UM FORMATO VIA QUERY PARAM
+        *  http://localhost:8080/api/person/v1?mediaType=xml
+        */
+//        configurer.favorParameter(true)
+//                .parameterName("mediaType").ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML);
+
+        /*
+        * CONFIG PARA OBTER UM FORMATO VIA HEADER PARAM
+        */
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML);
+
     }
 }
