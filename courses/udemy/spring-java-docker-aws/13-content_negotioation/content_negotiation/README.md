ce70d5f84b69a1e28b158b5e6ba2e4b7eba09691
João Vidal <dev.joaovidal@gmail.com>
2023-03-14 22:00:27 -0300

    spring-java-docker-aws: implementing content negotiation through query params

--- courses/udemy/spring-java-docker-aws/13-content_negotioation/content_negotiation/README.md
+++ courses/udemy/spring-java-docker-aws/13-content_negotioation/content_negotiation/README.md
@@ -1,9 +1,21 @@
-### Migrations
+### Content Negotioation
 
-> executar as migrations sem subir a aplicação: 'mvn clean package spring-boot:run -DskipTests'
+- O que é content negotiation?
+
+é a capacidade do servidor rest de prover diferentes objetos: Json, xml, yml, csv, pdf, png, etc..
+
+pode estar relacionado a idioma também, como por exemplo prover a response em diferentes idiomas.
+
+> Server-driven -> quem define a capacidade de prover os serviços
+
+> Agent-driven -> responsável por escolher como ele quer consumir, ou seja, quero consumir em json, yml ou csv. Sempre vai depender do que o servidor prove.
+
+- Content negotiation é a capacidade de servir diferentes formatos da mesma informação
+
+> implementaremos apenas a nível de formato: json, xml e yml.
+
+> sobre content negotiation e suas implementações -> https://www.baeldung.com/spring-mvc-content-negotiation-json-xml
 
 
-> após adicionar o plugin de flyway ao pom, basta executar no terminal o comando 'mvn flyway:migrate' para executar a migração do banco de dados sem precisar subir a aplicação
-> - um problema comum na migração com flyway é o hashcode não equivaler com o disposto no banco de dados, podendo ocorrer apenas por um espaço a mais ou a menos
-
-> no ambiente de testes não é problema apagar a tabela e iniciar uma nova migração do zero, caso seja necessário é possivel se aprofundar em migrações.
+> ### Acesso via query param:
+> http://localhost:8080/api/person/v1?mediaType=xml
--- courses/udemy/spring-java-docker-aws/13-content_negotioation/content_negotiation/src/main/java/com/example/integrando_database/controllers/PersonController.java
+++ courses/udemy/spring-java-docker-aws/13-content_negotioation/content_negotiation/src/main/java/com/example/integrando_database/controllers/PersonController.java
@@ -18,24 +18,24 @@ public class PersonController {
     private PersonServices service;
 
     // O swagger exige o mediatype, colocamos para não precisar adicionar lá na frente
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public List<PersonVO> findAll() {
         return service.findAll();
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public PersonVO findById(@PathVariable(value = "id") Long id) {
         return service.findById(id);
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public PersonVO create(@RequestBody PersonVO person) {
         return service.create(person);
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public PersonVO update(@RequestBody PersonVO person) {
         return service.update(person);
     }
