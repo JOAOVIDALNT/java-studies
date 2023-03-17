98abe649742f7aca80cbea7d30fb2fd9f2355fb7
= <=>
2023-03-17 09:18:58 -0300

    better way to solve needle challenge

--- challenges/codewars/_01-a_needle_in_the_haystack/src/Main.java
+++ challenges/codewars/_01-a_needle_in_the_haystack/src/Main.java
@@ -1,22 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Object[] haystack = {"test","test","test","needle","tiger"};
+
+        findNeedle(haystack);
     }
 
     public static String findNeedle(Object[] haystack) {
-        int position = -1;
-
-        for (int i = 0; i < haystack.length; i++) {
-            if(haystack[i] == "needle") {
-                position = i;
-            }
-        }
-        System.out.println(haystack);
-        if (position >= 0) {
-            return "found the neelde at position " + position;
-        } else {
-            return "we haven't found the needle";
-        }
-
+        return String.format("find needle at position, %d", Arrays.asList().indexOf("needle"));
     }
 }
