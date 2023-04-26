738bf188f9b96e798f90aa09e9ac76059013e7f2
João Vidal <dev.joaovidal@gmail.com>
2023-04-26 11:23:28 -0300

    feign tests

--- concepts/apiConsume/feign-via-cep-consume/src/main/java/com/example/feignviacepconsume/models/Address.java
+++ concepts/apiConsume/feign-via-cep-consume/src/main/java/com/example/feignviacepconsume/models/Address.java
@@ -17,79 +17,39 @@ public class Address {
         return cep;
     }
 
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
     public String getLogradouro() {
         return logradouro;
-    }
-
-    public void setLogradouro(String logradouro) {
-        this.logradouro = logradouro;
     }
 
     public String getComplemento() {
         return complemento;
     }
 
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
     public String getBairro() {
         return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
     }
 
     public String getLocalidade() {
         return localidade;
     }
 
-    public void setLocalidade(String localidade) {
-        this.localidade = localidade;
-    }
-
     public String getUf() {
         return uf;
-    }
-
-    public void setUf(String uf) {
-        this.uf = uf;
     }
 
     public String getIbge() {
         return ibge;
     }
 
-    public void setIbge(String ibge) {
-        this.ibge = ibge;
-    }
-
     public String getGia() {
         return gia;
-    }
-
-    public void setGia(String gia) {
-        this.gia = gia;
     }
 
     public String getDdd() {
         return ddd;
     }
 
-    public void setDdd(String ddd) {
-        this.ddd = ddd;
-    }
-
     public String getSiafi() {
         return siafi;
-    }
-
-    public void setSiafi(String siafi) {
-        this.siafi = siafi;
     }
 }
